35ec3457325255677c2217cfbbb8b98ccbc1e15e
Dailton Bastos <dailtonbastos@gmail.com>
2026-01-20 11:51:35 -0300

    test(categories): e2e categories response with pagination

--- test/categories.e2e.spec.ts
+++ test/categories.e2e.spec.ts
@@ -1,6 +1,8 @@
 import { HttpStatus } from '@nestjs/common'
+import { CategoryDto } from 'src/categories/dtos'
+import { Order, PAGINATION_META_DEFAULT_VALUES } from 'src/common/constants'
+import { PaginationOptionsDto } from 'src/common/pagination'
 import request from 'supertest'
-
 import { app } from './setup'
 
 describe('Categories (e2e)', () => {
@@ -9,7 +11,49 @@ describe('Categories (e2e)', () => {
 			const response = await request(app.getHttpServer())
 				.get('/api/categories')
 				.expect(HttpStatus.OK)
-			expect(response.body).toBeInstanceOf(Array)
+
+			expect(response.body.data).toBeInstanceOf(Array<typeof CategoryDto>)
+		})
+
+		it('should return an array of categories with pagination', async () => {
+			const paginationOptionsDto: PaginationOptionsDto = {
+				page: 1,
+				take: 10,
+				order: Order.ASC,
+				skip: 0
+			}
+
+			const response = await request(app.getHttpServer())
+				.get('/api/categories')
+				.query(paginationOptionsDto)
+				.expect(HttpStatus.OK)
+
+			expect(response.body.meta).toBeDefined()
+			expect(response.body.meta.hasNextPage).toBeDefined()
+			expect(response.body.meta.hasPreviousPage).toBeDefined()
+			expect(response.body.meta.page).toBeDefined()
+			expect(response.body.meta.take).toBeDefined()
+			expect(response.body.meta.itemCount).toBeDefined()
+			expect(response.body.meta.pageCount).toBeDefined()
+		})
+
+		it('should return an array of categories with default pagination options', async () => {
+			const response = await request(app.getHttpServer())
+				.get('/api/categories')
+				.expect(HttpStatus.OK)
+
+			expect(response.body.meta.hasNextPage).toBeFalsy()
+			expect(response.body.meta.hasPreviousPage).toBeFalsy()
+			expect(response.body.meta.page).toBe(
+				PAGINATION_META_DEFAULT_VALUES.options.page
+			)
+			expect(response.body.meta.take).toBe(
+				PAGINATION_META_DEFAULT_VALUES.options.take
+			)
+			expect(response.body.meta.itemCount).toBe(
+				PAGINATION_META_DEFAULT_VALUES.itemCount
+			)
+			expect(response.body.meta.pageCount).toBe(0)
 		})
 	})
 
