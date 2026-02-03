7b21b6e2bc508fe6c48fe1407a263ccacb9a4062
Dailton Bastos <dailtonbastos@gmail.com>
2026-02-03 09:08:14 -0300

    fix(auth.controller): replace get by post in register endpoint

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get } from '@nestjs/common'
+import { Body, Controller, Post } from '@nestjs/common'
 import { CreateUserDto } from 'src/users/dtos'
 import { User } from 'src/users/user.entity'
 import { AuthService } from './auth.service'
@@ -7,7 +7,7 @@ import { AuthService } from './auth.service'
 export class AuthController {
 	constructor(private readonly authService: AuthService) {}
 
-	@Get('register')
+	@Post('register')
 	async register(@Body() createUserDto: CreateUserDto): Promise<User> {
 		return this.authService.register(createUserDto)
 	}
