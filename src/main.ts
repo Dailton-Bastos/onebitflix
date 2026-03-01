555eca243dfd03ef54a2ae30bf70fbc610f2184e
Dailton Bastos <dailtonbastos@gmail.com>
2026-02-28 21:17:33 -0300

    chore(main): enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { AppModule } from './app.module'
 import { appConfig } from './config/app.config'
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+	const app = await NestFactory.create(AppModule, { cors: true })
 
 	appConfig(app)
 
